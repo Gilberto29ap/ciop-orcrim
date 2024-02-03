a71839150a0067babad7c369240eac16c6278911
Gilberto29ap <gilberto29ap@gmail.com>
2024-02-03 13:32:26 -0300

    add classe token para gerenciamento de token

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,5 @@
 
 client_id = 'SSPAP'
 client_secret = 'bc13f6c9-2183-47e7-a392-c3ac8c4f1080'
+token_url = "https://hmlsegurancaorcrim.mj.gov.br/auth/realms/hmlorcrim/protocol/openid-connect/token"
+scopes = 'openid profile orcrim-backend'
