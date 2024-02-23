e89c06ad9d4614e6b9b22383ddc1c5bcf08aa053
Gilberto29ap <gilberto29ap@gmail.com>
2024-02-23 12:39:00 -0300

    att para compartilhamento

--- config/__init__.py
+++ config/__init__.py
@@ -2,5 +2,5 @@
 
 # dados API orcrim
 
-client_id = 'SSPAP'
-client_secret = 'bc13f6c9-2183-47e7-a392-c3ac8c4f1080'
+# client_id = 'SSPAP'
+# client_secret = 'bc13f6c9-2183-47e7-a392-c3ac8c4f1080'
--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,6 @@
-
-client_id = 'SSPAP'
-client_secret = 'bc13f6c9-2183-47e7-a392-c3ac8c4f1080'
+from config.secrets import api_secret, id
+client_secret = api_secret
+client_id = id
 token_url = "https://hmlsegurancaorcrim.mj.gov.br/auth/realms/hmlorcrim/protocol/openid-connect/token"
 scopes = 'openid profile orcrim-backend'
 base_url = ""
